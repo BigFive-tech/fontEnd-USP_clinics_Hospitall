0d97cdd2e46e628db4cc8e66cef307532a557d7f
Lucas-B-Iori <lucasbertolassiiori@gmail.com>
2024-03-25 16:40:28 -0300

    Add modal e form

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,26 @@
+import { IoMdCloseCircle } from "react-icons/io";
+import styles from './Modal.module.css'
+
+export default function Modal({ fechaModal, videoUrl }) {
+  return(
+    <>
+      <div className={styles.fade} onClick={fechaModal}></div>
+      <div className={styles['modal-item']}>
+          <div className={styles["modal-item__header"]}>
+              <span classname={styles["close-modal"]} onClick={fechaModal}>
+                <IoMdCloseCircle />
+              </span>
+          </div>
+          <div className={styles["modal-item__body"]}>
+              <iframe 
+                width="100%" height="400" 
+                src={videoUrl} 
+                title="YouTube video player" frameBorder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                allowFullScreen> 
+              </iframe>
+          </div>
+      </div>
+    </>
+  )
+}
